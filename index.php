e2ab39dde36c3cd3368817b7b81f97705abeb523
Rithisak0511 <rithisak05112004@gmail.com>
2025-05-09 22:22:12 +0700

    update navbar

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body>
-    
+    <?php 
+    include 'view/navbar.php';
+    ?>
 </body>
 </html>
---
+++ view/navbar.php
@@ -0,0 +1,40 @@
+
+<?php
+    echo '
+        <nav class="bg-gray-800 p-4">
+            <div class="container mx-auto flex justify-between items-center">
+                <div class="text-white text-lg font-bold">My Website</div>
+                    <div class="hidden md:flex space-x-4>
+                        <ul class="flex space-x-8">
+                            <li>
+                                <a href="#" class="text-black hover:text-white">Home</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-black hover:text-white">About</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-black hover:text-white">Pricing</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-black hover:text-white">Contact</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-black hover:text-white">Contact</a>
+                            </li>
+                        </ul>                                    
+                    </div>
+                <div>
+                    <a href="#" class="text-white hover:text-gray-400">Login</a>
+                    <a href="#" class="ml-4 text-white hover:text-gray-400">Register</a>
+                </div>
+                <div class="md:hidden">
+                    <button class="text-gray-300 hover:text-white focus:outline-none">
+                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                        </svg>
+                    </button>
+            </div>
+        </div>
+    </nav>';
+?>
+
